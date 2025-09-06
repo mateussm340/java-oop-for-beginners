3bc211f098449a2606a1b753e50f07dd3d3ee986
mateussm340 <mateussm340@gmail.com>
2025-09-05 22:59:19 -0300

    feat: add main class to start the application

---
+++ CashewBank/src/Main.java
@@ -0,0 +1,8 @@
+import br.com.mateussm340.cashewbank.models.AccountClient;
+
+public class Main {
+    public static void main(String[] args) {
+        AccountClient accountClient = new AccountClient();
+        accountClient.showCustomerDataClient();
+    }
+}
